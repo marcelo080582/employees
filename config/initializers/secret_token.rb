3abdd85e701c1ced4577103a8f556680bfde3b02
Marcelo Martins <marcelo.martins@cilia.com.br>
2022-09-21 13:32:08 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Employees::Application.config.secret_token = '1d95aa4cb8038badea75ea14f4a72a7b75ab1aae789dc7894930ee714b6bbf66f42a5468031dcd4273a450944a04961722497fdd9723fdb00d03130892b1f4a3'
